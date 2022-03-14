3a2532bcdfba6ab6fcebbd314d5ac58bf5c8e091
pdguerra12 <paul.guerra09@gmail.com>
2022-03-14 13:31:09 -0500

    initialized app

---
+++ src/components/About/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+// React.createElement("section"), {}, [];
+// React.createElement("h1"), {}, ["Who am I?"];
+
+function About() {
+	return (
+		<section className="my-5">
+			<h1 id="about">Who am I?</h1>
+			<img
+				src={coverImage}
+				className="my-2"
+				style={{ width: "100%" }}
+				alt="cover"
+			/>
+		</section>
+	);
+}
+
+export default About;
